4fbb4cd98129578e9aa130c113b6bbac2e329d1b
jaedeokhan <hjaedeok@gmail.com>
2023-03-15 23:58:30 +0900

    ADD : 회원 관리 웹 애플리케이션 요구사항 - MemberRepository 구현 - MemberRepository 테스트 코드

---
+++ src/test/java/hello/servlet/domain/member/MemberRepositoryTest.java
@@ -0,0 +1,52 @@
+package hello.servlet.domain.member;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MemberRepositoryTest {
+
+    MemberRepository memberRepository = MemberRepository.getInstance();
+
+    @AfterEach
+    void afterEach() {
+        memberRepository.clearStore();
+    }
+
+    @Test
+    @DisplayName("회원 저장 테스트")
+    void save() {
+        // given
+        Member member = new Member("hello", 20);
+
+        // when
+        Member savedMember = memberRepository.save(member);
+
+        // then
+        Member findMember = memberRepository.findById(savedMember.getId());
+        Assertions.assertThat(savedMember).isEqualTo(findMember);
+    }
+
+    @Test
+    @DisplayName("회원 전체 조회")
+    void findAll() {
+        // given
+        Member member1 = new Member("member1", 20);
+        Member member2 = new Member("member2", 30);
+
+        memberRepository.save(member1);
+        memberRepository.save(member2);
+
+        // when
+        List<Member> memberList = memberRepository.findAll();
+
+        // then
+        Assertions.assertThat(memberList.size()).isEqualTo(2);
+        Assertions.assertThat(memberList).contains(member1, member2);
+    }
+}
