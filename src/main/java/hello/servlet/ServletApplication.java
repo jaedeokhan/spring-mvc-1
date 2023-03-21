7fbb5c391823bf0eecc0cfc013083b6b11c44dc8
jaedeokhan <hjaedeok@gmail.com>
2023-03-21 22:22:07 +0900

    ADD : 뷰 리졸버 - 스프링 부트는 해당 클래스를 `InternalResourceViewResolver` 를 자동으로   등록하는데, 이때 application.properties에 등록한   spring.mvc.view.prefiex, suffix를 설정 정보로 사용해서 등록한다. - 다른 뷰는 실제 뷰를 렌더링하지만, JSP의 경우에는 forward()통해서 해당   JSP로 이동해야 렌더링된다.

--- src/main/java/hello/servlet/ServletApplication.java
+++ src/main/java/hello/servlet/ServletApplication.java
@@ -3,6 +3,11 @@ package hello.servlet;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.view.InternalResourceView;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+import java.beans.BeanProperty;
 
 @ServletComponentScan // 서블릿 자동 등록
 @SpringBootApplication
